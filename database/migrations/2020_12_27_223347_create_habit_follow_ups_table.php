0f7bc48322057d818e0572916ebb880cde82e457
Alejandro <alejandro.devop@gmail.com>
2020-12-27 17:55:12 -0500

    Added follow ups to habits

---
+++ database/migrations/2020_12_27_223347_create_habit_follow_ups_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateHabitFollowUpsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('habit_follow_ups', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('habit_id')
+                ->references('id')
+                ->on('habits');
+            $table->dateTime('apply_date');
+            $table->text('story');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('habit_follow_ups');
+    }
+}
